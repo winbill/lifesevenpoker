ef14a79e985abd788480f11f9809d363f190f3c0
delys.olivier <delys.olivier@1204c978-5248-0410-81a0-0bcb81ba3628>
2008-04-01 09:03:51 +0000

    Ajout de Carte.C

---
+++ Carte.C
@@ -0,0 +1,53 @@
+#include "Carte.h"
+
+
+void initialisationCarte(Carte & c)
+{
+	c.couleur = INIT_COULEUR;
+	c.rang = INIT_RANG;
+}
+
+Rang getCarteRang(const Carte & c)
+{
+	return c.rang;
+}
+
+Couleur getCarteCouleur(const Carte & c)
+{
+	return c.couleur;
+}
+
+/*Carte getCarte(const Carte & c)
+{
+	return c;
+}
+*/
+
+void setCarte(Carte & c,const Couleur & couleur,const Rang & rang)
+{
+	c.couleur = couleur;
+	c.rang = rang;
+}
+
+int compareCarte(const Carte & c1, const Carte & c2)
+{
+	Rang rang1=getCarteRang(c1);
+	Rang rang2=getCarteRang(c2);
+	
+	if(rang1<rang2)
+	{
+		return 1;
+	}
+	else if(rang1>rang2)
+	{
+		return -1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+//void testamentCarte(Carte & c);
+
+
