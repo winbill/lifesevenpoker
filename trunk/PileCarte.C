c9cfd65c560494523d3dd177f731c8ab70fcde56
jamesdgb <jamesdgb@1204c978-5248-0410-81a0-0bcb81ba3628>
2008-04-01 16:23:51 +0000

    nouvelle fonction

--- trunk/PileCarte.C
+++ trunk/PileCarte.C
@@ -1,3 +1,8 @@
+/**	@brief module PileCarte.C
+	@author James
+	@version 1.0
+        @date 2008/04/01
+*/
 #include "Carte.h"
 #include "PileCarte.h"
 #include "assert.h"
@@ -12,6 +17,38 @@
 
 void initPileCarte (PileCarte & pileCarte)
 {
+	int i;
+	int j;
+	int k=0;
+	for(i=1;i<=4;i++)
+	{
+		for(j=2;j<=14;j++)
+		{
+			carte = new Carte;
+			setCarte(carte,i,j);
+			pileCarte.pileDesCartes[k] = *carte;
+			k++;
+		}
+	}
 	
+}
+
+Carte* recevoirCarte(PileCarte & pileCarte)
+{
+	int x = hasard(0,pileCarte.taille-1);
+	carte = new Carte;
+	carte* = pileCarte.pileDesCartes[x];
+	//ou carte  ?????
+	
+	//on permute pour que la carte soit a la fin (apres le rang taille -2 du tableau)
+	if(x!=pileCarte.taille-1)
+		pileCarte.pileDesCartes[x] = pileCarte.[pileCarte.taille-1];
+	
+	//on enleve une carte a la taille, mais la carte est toujours présente dans le tableau
+	pileCarte.taille --;
+	
+	
+	return carte
+	//ou *carte ??
 	
 }
--- trunk/PileCarte.h
+++ trunk/PileCarte.h
@@ -1,4 +1,4 @@
-/**	@brief module Table.h
+/**	@brief module PileCarte.h
 	@author James
 	@version 1.0
         @date 2008/04/01
@@ -12,7 +12,7 @@
 
 /**	@brief structure representant le jeu (de 52 cartes au depart) dou sont distribuees les cartes
 	@param pileDesCartes tableau contenant les cartes
-	@param taille taille du tableau des cartes (au depart dune partie 52)
+	@param taille du tableau des cartes (au depart dune partie 52)
 */
 struct PileCarte
 {
@@ -20,13 +20,29 @@ struct PileCarte
 	int taille;
 };
 
-/**	@brief initialise la pile de carte
+/**	@brief initialise la pile de carte, en creant 52 cartes dans le tas
+	@note ne melange pas les cartes
 */
 void initPileCarte (PileCarte & pileCarte);
 
 /**	@brief detruit la pile de carte proprement
 */
 void testamentPileCarte (PileCarte & pileCarte);
+
+/**	@brief sort une carte au hasard de pileDesCartes
+
+	ne la supprime pas du tableau donc pas la peine de reinitialiser
+	pileCarte a chaque debut de partie ,il suffit de mettre la taille a 52
+	@note utilise la fonction hasard
+*/
+Carte* recevoirCarte(PileCarte & pileCarte);
+
+/**	@brief renvoi un nombre au hasard dans l'intervalle (inclus)
+	@param x le plus petit chiffre qui peut etre renvoye
+	@param y le plus grand chiffre qui peut etre renvoye
+	@return entier aleatoire compris entre x et y (compris)
+*/
+int hasard(x,y);
 
 
 
