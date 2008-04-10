f9435fd936f300c10c1c6eedd71faac8e69d0331
jamesdgb <jamesdgb@1204c978-5248-0410-81a0-0bcb81ba3628>
2008-04-10 13:54:34 +0000

    oublit de la memorisation du dealer dans table

--- Table.h
+++ Table.h
@@ -118,6 +118,9 @@ void setJoueurTable (Joueur* & joueur, Table & table);
 */
 int placeVide (const Table & table);
 
+/**	@brief change la position du dealer (decale de un)
+*/
+void changeDealerTable(Table & table);
 
 
 
