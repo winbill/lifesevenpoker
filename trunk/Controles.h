1255d8f38b34f7c83e7f87c43f30771cb1a68a81
jamesdgb <jamesdgb@1204c978-5248-0410-81a0-0bcb81ba3628>
2008-04-10 14:21:05 +0000

    debut de controle.H

---
+++ trunk/Controles.h
@@ -0,0 +1,28 @@
+#ifndef Controles_H
+#define Controles_H
+
+
+/**	@brief demande au joueur ce qu il veut faire
+	@note change eventuellement son statut
+	@return peut retourner le montant de ca relance
+*/
+int controleTActionJoueur(Joueur& joueur);
+
+/**	@brief permet d executer des commandes pour le beboguage
+	@note comme voir les cartes de d'autres joueur
+*/
+void controleTDebugMode();
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
