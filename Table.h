140037e429a95bbf1c3c8125701690d5300bdce4
jamesdgb <jamesdgb@1204c978-5248-0410-81a0-0bcb81ba3628>
2008-04-11 13:01:14 +0000

    precision des commentaire pour doxygen

--- Table.h
+++ Table.h
@@ -30,7 +30,9 @@
 #include "Joueur.h"
 
 
-/**	@brief structure representant la table du jeu de poker, maximum 10 joueurs
+/**
+	@struct Table
+	@brief structure representant la table du jeu de poker, maximum 10 joueurs
 	@param nJoueur nombre de joueurs
 	@param joueur tableau de pointeur de joueurs
 	@param nMaxJoueur nombre de joueur maximale (pour le reseau apres)
@@ -51,18 +53,22 @@ struct Table
 
 
 
-/**	@brief initialise la variable table
+/**	
+	@brief initialise la variable table
+	@param [in, out] table une table
 */
 void initTable (Table & table);
 
 
 /**	@brief initialise la variable table
+	@param [in, out] table une table
 	@param nJoueur nombre de joueur souhaite et nombre de joueur maximal
 */
 void initTable (Table & table,int nJoueur);
 
 
 /**	@brief initialise lune variable table
+	@param [in, out] table une table
 	@param nJoueur nombre de joueur souhaite et nombre de joueur maximal
 	@param pileCarte adresse de la pile de carte
 */
@@ -77,18 +83,21 @@ Table* creeTable();
 
 
 /**	@brief libere les allocations internes a table
+	@param [in] table une table
 */
 void tableLibere(Table & table);
 
 
 /**	@brief libere plus detruit la structure
-	@param table adresse d une table
+	@param [in, out] table adresse d une table
 	@note appel la fonction tableLibere
 */
 void tableDetruit(Table* & table);
 
 
 /**	@brief Ajoute un joueur a la table
+	@param [in, out] table une table
+	@param [in] joueur un joueur
 	@note change table.nJoueur en incrementant de 1
 */
 void ajoutJoueurTable (Table & table,const Joueur* joueur);
@@ -96,51 +105,63 @@ void ajoutJoueurTable (Table & table,const Joueur* joueur);
 
 /**	@brief supprime le joueur du tableau en effectuant une recherche
 	le joueur existe encore apres
-	@param joueur pointeur vers joueur
+	@param [in, out] table une table
+	@param [in] joueur pointeur vers joueur
 */
 void supprimeJoueurTable (Table & table,const Joueur* joueur);
 
 
 /**	@brief renvoi le rang dans le tableau du Joueur
-	@param joueur pointeur vers joueur
+	@param [in] joueur pointeur vers joueur
+	@param [in, out] table une table
 	@warning affiche un message derreur sur la sortie standart si aucun joueur correspondant n'a ete trouve
 */
 int joueurTrouver (Table & table,const Joueur* joueur);
 
 
 /**	@brief etablit un nombre de joueurs maximal (pour le reseau)
+	@param [in, out] table une table
 	@param n nombre de joueur maximum
 */
 void setMaxJoueurTable (Table & table, int n);
 
 
 /**	@brief renvoi le nombre de joueurs maximal
+	@param [in] table une table
 	@return nombre de joueur max
 */
 int getMaxJoueurTable (const Table & table);
 
 
 /**	@brief renvoi l adresse de la pile de carte
+	@param [in] table une table
+	@return l'adresse de la pile de carte utilise a la table
 */
 PileCarte* getTablePileCarteTable (const Table & table);
 
 
 /**	@brief etablit un nombre de joueurs n a la table
-	@param n nombre de joueurs desires
+	@param [in, out] table
+	@param [in] n nombre de joueurs desires
 */
 void setNJoueurTable (Table & table, int n);
 
 
 /**	@brief renvoi le nombre de joueurs
+	@param [in] table une table
+	@return nombre de joueur a la table
 */
 int getNJoueurTable (const Table & table);
 
 
-/**     @brief renvoi le numero de la premiere place libre, -1 sinon
+/**	@brief cherche une place vide dans le tableau de joueur
+	@param [in] table une table
+	@return renvoi l'indice de la premiere place libre, -1 sinon
 */
 int placeVide (const Table & table);
 
-/**     @brief change la position du dealer (decale de un)
+/**	@brief change la position du dealer (decale de un)
+	@param [in, out] table une table
 */
 void changeDealerTable(Table & table);
 
