62e636935e92de14939f3a8f84ad008b109c5722
bengrunt <bengrunt@1204c978-5248-0410-81a0-0bcb81ba3628>
2008-04-15 08:27:39 +0000

    maj du script checkout

--- trunk/checkout.sh
+++ trunk/checkout.sh
@@ -2,6 +2,10 @@
 #Penser à modifier son login.
 #Ecrit par Benjamin.
 
+
+#A utiliser dans la directorie ou est fait le checkout habituellement.
+#Par exemple si vous travaillez dans /hone/xxx/lifesevenpoker/ il faut executer ce script dans /home/xxx/   c'est à dire dans votre répertoire de #connection.
+
 svn checkout https://lifesevenpoker.googlecode.com/svn/trunk/ lifesevenpoker --username bengrunt
 
 codeblocks LifeSevenPoker.cbp &
