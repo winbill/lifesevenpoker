e9ebd1521de1984324819f7c3a5a80a447eed054
jamesdgb <jamesdgb@1204c978-5248-0410-81a0-0bcb81ba3628>
2008-04-15 16:08:57 +0000

    petits ajouts

---
+++ trunk/main.C
@@ -0,0 +1,24 @@
+#include "Jeu.h"
+
+
+
+void main()
+{
+    SDL_Event event;
+
+    //afficheSdlIntro();
+
+    //choix des options nombre de joueur, difficultés etc ...
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
