baac083fa31557bce0987dd01ceb974d388ddc0d
bengrunt <bengrunt@1204c978-5248-0410-81a0-0bcb81ba3628>
2008-04-21 09:34:00 +0000

    idem

--- AfficheDynamique.C
+++ AfficheDynamique.C
@@ -16,11 +16,40 @@
 
 //Librairies SDL.
 #include <SDL/SDL.h>
-#include "SDL/SDL_image.h" //Gestion des images.
-#include "SDL/SDL_ttf.h" //Gestion des polices True Type Fonts.
-#include "SDL/SDL_mixer.h" //Gestion du multi channeling audio.
+#include <SDL/SDL_image.h> //Gestion des images.
+#include <SDL/SDL_ttf.h> //Gestion des polices True Type Fonts.
+#include <SDL/SDL_mixer.h> //Gestion du multi channeling audio.
 
 
+bool init(SDL_Surface* & screen, const int & screen_width, const int & screen_height, const int & screen_bpp, std::string caption)
+{
+	//Initialisation de tous les sous-systèmes de SDL
+	if( SDL_Init( SDL_INIT_EVERYTHING ) == -1 )
+	{
+		return false;
+	}
+
+	//Mise en place de l'écran
+	screen = SDL_SetVideoMode( screen_width, screen_height, screen_bpp, SDL_SWSURFACE );
+
+	//S'il y a une erreur dans la mise en place de l'écran
+	if( screen == NULL )
+	{
+		return false;
+	}
+
+	//Initialisation de SDL_TTF
+	if( TTF_Init() == -1 )
+	{
+		return false;
+	}
+
+	//Mise en place de la barre de titre de la fenetre principale (caption)
+	SDL_WM_SetCaption( caption , NULL );
+
+	//Si tout s'est bien passé
+	return true;
+}
 
 SDL_Surface *load_image( std::string filename )
 {
