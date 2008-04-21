767692ec8aa9c043c06e5d032618d1025162f578
masse501e <masse501e@hotmail.com>
2008-04-21 21:15:13 +0000

    changement nom des cartes

---
+++ Joueur.C
@@ -0,0 +1,12 @@
+/**
+	@brief Fichier C pour les cartes.
+
+	Gere les donnees de type Carte.
+
+	@author Tristan, .......
+	@file Joueur.h
+	@version 1.0
+	@date 2008/04/21
+*/
+
+#include "Joueur.h"
