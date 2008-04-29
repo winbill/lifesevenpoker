d756a9ce993c71472290817b3faacf0e052e5217
jamesdgb <jamesdgb@1204c978-5248-0410-81a0-0bcb81ba3628>
2008-04-29 08:57:14 +0000

    James et Tristan changement codageScoreMain

--- MainCarte.C
+++ MainCarte.C
@@ -151,16 +151,17 @@ void MainCarteLibere(MainCarte & m)
 	m.nCarte=0;
 }
 
-
-int couleurMainCarte(const int & tab7Carte[8][2])
+int couleurMainCarte(int tab7Carte[8][2])
 {
     char[4] c;
     c = {0,0,0,0}; //tableau qui va contenir le nombre de carte de chaque couleur
+    //on compte le nombre de carte de chaque couleur
     for(int i=0;i<=7;i++)
     {
         c[tab7Carte[i][1]-1] ++; //on incremente chaque celulle correspondant a la couleur
                                                     //du tableau de un
     }
+    //on regarde sil y en a une superieur a 5
     i=0;
     while(i<4)
     {
@@ -168,31 +169,69 @@ int couleurMainCarte(const int & tab7Carte[8][2])
             return i+1; //s'il y a une couleur presente 5 fois on renvoit son code
     }
     return 0
-
 }
 
-int suiteMainCarte2(const int & tab7Carte[8][2],int i,int j,int l)
+void trieTableauRang(int tab7Carte[7][2])
 {
-    if(j==4)
+    int temp,i,k;
+    for(k=0,k<7,k++)
     {
-        return (tab7Carte[i-4][0]);
+        for(i=0,i<7,i++)
+        {
+            if(tab7Carte[i][0]<tab7Carte[i+1][0])
+            {
+                tab7Carte[i][0]=temp; //temp du rang
+                tab7Carte[i][0]=tab7Carte[i+1][0];
+                tab7Carte[i+1][0]=temp;
+                tab7Carte[i][1]=temp; //temp de la couleur
+                tab7Carte[i][1]=tab7Carte[i+1][1];
+                tab7Carte[i+1][1]=temp;
+            }
+        }
     }
-    //s'il reste qu'une carte et que j != 4 , il ne peut pas avoir de suite
-    if(i==l-1)
-        return 0;
+}
 
-    if(tab7Carte[i][0] == tab7Carte[i+1][0]+1)
+
+
+
+//l a besoin d1 tab trie DEC
+int suiteMainCarte(int tab7Carte[8][2])
+{
+    int k=7;// variable qui indique le nombre de carte
+
+    //s'il y a un (ou plus) AS on le rajoute a la fin
+    if(tab7Carte[0][0] == 14)
     {
-        suiteMainCarte(tab7Carte,i+1,j+1,l);
-    }else if(tab7Carte[i][0] == tab7Carte[i+1][0]) //cas 9 10 10 J Q R
-    {
-        suiteMainCarte(tab7Carte,i+1,j,l);
-    }else if(tab7Carte[i][0] == 0)   //pour la quite flush (carte nulle)
-    {
-        suiteMainCarte(tab7Carte,i+1,j,l);
-    }else{
-        suiteMainCarte(tab7Carte,i+1,0,l);
+        k = 8;
+        tab7Carte[7][0] = 1;
+        tab7Carte[7][1] = tab7Carte[0][1];
     }
+
+    int nCarteConsecutive = 0;
+    int compteurCarte = 0;
+    while(nCarteConsecutive != 4 && compteurCarte < (k-1))
+    {
+        //test si les deux cartes se suivent
+        if(tab7Carte[compteurCarte][0] == tab7Carte[compteurCarte+1][0]+1)
+        {
+            nCarteConsecutive++;
+        //si elle sont egales on ne fait rien
+        }else if(tab7Carte[compteurCarte][0] == tab7Carte[compteurCarte+1][0]){
+        }
+        //si elle sont differentes on remet le compteur a 0
+        else{
+            nCarteConsecutive=0;
+            if(k-compteurCarte<6) //on test sil est encore possible de faire une suite
+                return 0;
+        }
+        k++;
+    }
+
+    if(nCarteConsecutive>=4)
+    {
+        return tab7Carte[compteurCarte][0]+3;//si suite on renvoit le rang de la meilleure carte de la suite
+    }
+    return 0;
 }
 
 
@@ -280,11 +319,11 @@ void nombreOcurenceCarte(const int & tab7Carte[10][2],int & tab[])
 }
 
 
-void choixCarteMultiple(const int & tab[],int & tabResultat[6])
+void choixCarteMultiple(int tab7Carte[7][2],const int & tab[],int & tabResultat[6][2])
 {
     //double triage du tableau tab
         //on trie dabord en fonction de tab[i] puis en fonction de i dans un tableau a 2d tab[x][y]
-        //de sorte que tab[x][0] contient un entier compris entre 0 et 4 (trié d'abord par rapport a x puis a y)
+        //de sorte que tab[x][0] contient un entier compris entre 0 et 4 (trie d'abord par rapport a x puis a y)
         //et tab[x][1] contient le rang de la carte
         //exemple : tab[1][0] = 2  tab[1][1] = 6  => une paire de 6 en 2e position
         //donc tab[0][0] > 2 ou tab[0][1] >6 => soit un brelan ou mieu soit une paire de 7 ou+
@@ -292,17 +331,30 @@ void choixCarteMultiple(const int & tab[],int & tabResultat[6])
 
 
 
-
+    int i;
     //a lissue de la boucle on a toutes les informations pour donner un nom et des valeurs a la main:
     switch (tab[0][0])
     {
         case 4:
-            tabResultat[0]=7;
-            tabResultat[1]=tab[0][1];
-            tabResultat[2]=tab[0][1];
-            tabResultat[3]=tab[0][1];
-            tabResultat[4]=tab[0][1];
-            tabResultat[5]=tab[1][1];
+            tabResultat[0][0]=7;
+            tabResultat[1][0]=tab[0][1];
+            tabResultat[1][1]=1;
+            tabResultat[2][0]=tab[0][1];
+            tabResultat[2][1]=2;
+            tabResultat[3][0]=tab[0][1];
+            tabResultat[3][1]=3;
+            tabResultat[4][0]=tab[0][1];
+            tabResultat[4][1]=4;
+
+            //on cherche la couleur de la carte correspondant au rang
+            i=0;
+            while(tab7Carte[i][0] != tab[1][1])
+            {
+                i++
+            }
+            tabResultat[5][0]=tab[1][1];
+            tabResultat[5][1]=tab7Carte[i][1];
+
         break;
         case 3:
             if(k+tab[l+1][0] >=2)
