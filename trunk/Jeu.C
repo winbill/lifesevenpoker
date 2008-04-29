3a87b52885423af6f6b7dcae16c944583093c7ba
jamesdgb <jamesdgb@1204c978-5248-0410-81a0-0bcb81ba3628>
2008-04-29 08:57:14 +0000

    James et Tristan changement codageScoreMain

--- trunk/Jeu.C
+++ trunk/Jeu.C
@@ -125,97 +125,261 @@ void lanceTourJeu(Table & table)
 
 
 
-
-int codageScoreMain(const MainCarte &m, int & tabResultat[6],const Table & table)
+int couleurMainCarte(int tab7Carte[8][2])
 {
-    int i;
-    //on utilise un tableau a 2 dimensions representant les 7 cartes (8 car l'as peut etre rajouter une deuxieme fois)
-    int tabMainTotale[8][2];
-    //on recupere les deux cartes du joueur
-    tabMainTotale[0][0] = getCarteRang(*(m.tabMain[0]));
-    tabMainTotale[0][1] = getCarteCouleur(*(m.tabMain[0]));
-    tabMainTotale[1][0] = getCarteRang(*(m.tabMain[0]));
-    tabMainTotale[1][1] = getCarteCouleur(*(m.tabMain[0]));
-    //plus les 5 cartes decouvertes
-    for(i=0;i<5;i++)
+    char[4] c;
+    c = {0,0,0,0}; //tableau qui va contenir le nombre de carte de chaque couleur
+    //on compte le nombre de carte de chaque couleur
+    for(int i=0;i<=7;i++)
     {
-        tabMainTotale[i+2][0]=getCarteRang((*(table.carteDecouverte[i])))
-        tabMainTotale[i+2][1]=getCarteCouleur((*(table.carteDecouverte[i])))
+        c[tab7Carte[i][1]-1] ++; //on incremente chaque celulle correspondant a la couleur
+                                                    //du tableau de un
     }
-
-
-    //regle : si on a une couleur on a au mieux une quinte flush
-    //regle : si on a une quinte on a au mieux une quinte flush ou une couleure ou au pire une quinte
-    //on test la suite
-    int couleur=0;
-    int quinte=0;
-    int k=0;
-
-    //on regarde en 1er s'il y a une couleure
-    couleur = couleurMainCarte(tabMainTotale);
-    //one ragarde s'il y a une quinte
-    quinte = suiteMainCarte(tabMainTotale);
-
-    //sil y a une quinte et une couleur on regarde s'il y a une quinte flush
-    if((couleur!=0)&&(quinte!=0))
+    //on regarde sil y en a une superieur a 5
+    i=0;
+    while(i<4)
     {
-        int tabMainTotale2[10][2];
-        strcpy(tabMainTotale2,tabMainTotale); //on fait une copie du tableau
-        k=quinteFlushMainCarte(tabMainTotale2,couleur);
-        if(k!=0)
-        {
-            tabResultat[0] = 8;
-            tabResultat[1] = k;
-            tabResultat[2] = k-1;
-            tabResultat[3] = k-2;
-            tabResultat[4] = k-3;
-            tabResultat[5] = k-4;
+        if(c[i] >= 5)
+            return i+1; //s'il y a une couleur presente 5 fois on renvoit son code
+    }
+    return 0
+}
 
+void trieTableauRang(int tab7Carte[7][2])
+{
+    int temp,i,k;
+    for(k=0,k<7,k++)
+    {
+        for(i=0,i<7,i++)
+        {
+            if(tab7Carte[i][0]<tab7Carte[i+1][0])
+            {
+                tab7Carte[i][0]=temp; //temp du rang
+                tab7Carte[i][0]=tab7Carte[i+1][0];
+                tab7Carte[i+1][0]=temp;
+                tab7Carte[i][1]=temp; //temp de la couleur
+                tab7Carte[i][1]=tab7Carte[i+1][1];
+                tab7Carte[i+1][1]=temp;
+            }
         }
     }
-    //s'il y a qu'une couleure c'est la meillleur possibilité
-    if(couleur != 0)
+}
+
+
+
+
+//l a besoin d1 tab trie DEC
+int suiteMainCarte(int tab7Carte[8][2])
+{
+    int k=7;// variable qui indique le nombre de carte
+
+    //s'il y a un (ou plus) AS on le rajoute a la fin
+    if(tab7Carte[0][0] == 14)
     {
-        tabResultat[0] = 5;
-        //on doit prendre les 5 meilleurs cartes de la couleur correspondante
-        tabResultat[1] = ;
-        tabResultat[2] = k-1;
-        tabResultat[3] = k-2;
-        tabResultat[4] = k-3;
-        tabResultat[5] = k-4;
-
-
-
-        return 1; //on renvoit 1 pour quitter la fonction
-
+        k = 8;
+        tab7Carte[7][0] = 1;
+        tab7Carte[7][1] = tab7Carte[0][1];
     }
-    //s'il y a qu'une suite c'est la meilleur possibilité
-    if(quinte != 0)
-    {
-        tabResultat[0] = 4;
-        //on doit prendre les 5 cartes de la suite
-        // /!\ il faut verifier que yen ai pas deux egale
-        // cas 10 9 9 8 7 6 2 1 -> 10 9 8 7 6 et non 10 9 9 8 7
-        ...;
-        tabResultat[1] =quinte;
-        tabResultat[2] =quinte-1;
-        tabResultat[3] =quinte-2;
-        tabResultat[4] =quinte-3;
-        tabResultat[5] =quinte-4;
 
-        return 1;//on renvoit 1 pour quitter la fonction
+    int nCarteConsecutive = 0;
+    int compteurCarte = 0;
+    while(nCarteConsecutive != 4 && compteurCarte < (k-1))
+    {
+        //test si les deux cartes se suivent
+        if(tab7Carte[compteurCarte][0] == tab7Carte[compteurCarte+1][0]+1)
+        {
+            nCarteConsecutive++;
+        //si elle sont egales on ne fait rien
+        }else if(tab7Carte[compteurCarte][0] == tab7Carte[compteurCarte+1][0]){
+        }
+        //si elle sont differentes on remet le compteur a 0
+        else{
+            nCarteConsecutive=0;
+            if(k-compteurCarte<6) //on test sil est encore possible de faire une suite
+                return 0;
+        }
+        k++;
     }
-    //s'il a ni couleur ni quinte on regarde le reste
 
-    int tabOccurence[15];       //on cree le tableau d'occurence
-    nombreOcurenceCarte(tabMainTotale,tabOccurence);    //on met dans un tableau le nombre d'occurence de chaque carte
-    choixCarteMultiple(tabOccurence,tabResultat);     //on obtient directement le code dans tabResultat
-    if(tabResultat[0] >= 0)
+    if(nCarteConsecutive>=4)
     {
-        return 1;
+        return tab7Carte[compteurCarte][0]+3;//si suite on renvoit le rang de la meilleure carte de la suite
+    }
+    return 0;
+}
+
+
+int suiteMainCarte(int tab7Carte[8][2])
+{
+    //on trie le tableau en fonction du RANG (DECROISSANT)
+    int temp,i,k;
+    for(k=0,k<7,k++)
+    {
+        for(i=0,i<7,i++)
+        {
+            if(tab[i][0]<tab[i+1][0])
+            {
+                tab[i][0]=temp; //temp du rang
+                tab[i][0]=tab[i+1][0];
+                tab[i+1][0]=temp;
+                tab[i][1]=temp; //temp de la couleur
+                tab[i][1]=tab[i+1][1];
+                tab[i+1][1]=temp;
+            }
+        }
+    }
+
+    if(tab[0][0]!=14) //test pour savoir s'il y a un as
+    {
+        return suiteMainCarte2(tab7Carte,0,0,7);
     }else{
-        return 0;
+        tab[7][0] = 1;//on rajoute un 1 (as) pour LA suite du type (1 2 3 4 5)
+        tab[7][1] = 1;//on se fout de la couleur de l'as
+        int k = suiteMainCarte2(tab7Carte,0,0,8);
+        //on enleve l'as rajoute
+        tab[7][1]=0;
+        tab[7][0] = 0;
+        return k;
     }
 
+
+}
+
+
+
+int quinteFlushMainCarte(int tab7Carte[10][2],int couleur);
+{
+    int l =7;
+    //on met a zero les cartes qui ne sont pas de la couleur dominante
+    int i;
+    for(i=0;i<7;i++)
+    {
+        if(tab7Carte[i][1]!=couleur)
+        {
+            tab7Carte[i][1]=0;
+            tab7Carte[i][0]=0;
+        }
+    }
+    //on rajoute sil y a un AS un "1" a la fin du tableau
+    if(tab7Carte[0][0] == 14)
+    {
+        tab7Carte[7][0]=1;
+        l++;
+    }else if(tab7Carte[1][0] == 14)
+    {
+        tab7Carte[7][0]=1;
+        l++;
+    }else if(tab7Carte[2][0] == 14)
+        tab7Carte[7][0]=1;
+        l++;
+    }
+
+    return suiteMainCarte2(tab7Carte,0,0,l);
+
+
+}
+
+
+
+void nombreOcurenceCarte(const int & tab7Carte[10][2],int & tab[])
+{
+    int i;
+    for(i=0;i<15;i++)
+        tab[i] = 0;
+    for(i=0;i<7;i++)
+    {
+        tab[tab7Carte[i][0]]++;
+    }
+}
+
+
+void choixCarteMultiple(int tab7Carte[7][2],const int & tab[],int & tabResultat[6][2])
+{
+    //double triage du tableau tab
+        //on trie dabord en fonction de tab[i] puis en fonction de i dans un tableau a 2d tab[x][y]
+        //de sorte que tab[x][0] contient un entier compris entre 0 et 4 (trie d'abord par rapport a x puis a y)
+        //et tab[x][1] contient le rang de la carte
+        //exemple : tab[1][0] = 2  tab[1][1] = 6  => une paire de 6 en 2e position
+        //donc tab[0][0] > 2 ou tab[0][1] >6 => soit un brelan ou mieu soit une paire de 7 ou+
+            //A FAIRE  (il va de 0 a 14)
+
+
+
+    int i;
+    //a lissue de la boucle on a toutes les informations pour donner un nom et des valeurs a la main:
+    switch (tab[0][0])
+    {
+        case 4:
+            tabResultat[0][0]=7;
+            tabResultat[1][0]=tab[0][1];
+            tabResultat[1][1]=1;
+            tabResultat[2][0]=tab[0][1];
+            tabResultat[2][1]=2;
+            tabResultat[3][0]=tab[0][1];
+            tabResultat[3][1]=3;
+            tabResultat[4][0]=tab[0][1];
+            tabResultat[4][1]=4;
+
+            //on cherche la couleur de la carte correspondant au rang
+            i=0;
+            while(tab7Carte[i][0] != tab[1][1])
+            {
+                i++
+            }
+            tabResultat[5][0]=tab[1][1];
+            tabResultat[5][1]=tab7Carte[i][1];
+
+        break;
+        case 3:
+            if(k+tab[l+1][0] >=2)
+            {
+                tabResultat[0]=6;
+                tabResultat[1] =tab[0][1];
+                tabResultat[2] =tab[0][1];
+                tabResultat[3] =tab[0][1];
+                tabResultat[4] = tab[1][1];
+                tabResultat[5] = tab[1][1];
+            }else{
+                tabResultat[0]=3;
+                tabResultat[1] =tab[0][1];
+                tabResultat[2] =tab[0][1];
+                tabResultat[3] =tab[0][1];
+                tabResultat[4] =tab[1][1];
+                tabResultat[5] =tab[2][1];
+            }
+        break;
+        case 2:
+            if(k+tab[l+1][0] ==2)
+            {
+                tabResultat[0]=2;
+                tabResultat[1] =tab[0][1];
+                tabResultat[2] =tab[0][1];
+                tabResultat[3] =tab[1][1];
+                tabResultat[4] =tab[1][1];
+                tabResultat[5] =tab[2][1];
+            }else{
+                tabResultat[0]=1;
+                tabResultat[1] =tab[0][1];
+                tabResultat[2] =tab[0][1];
+                tabResultat[3] =tab[1][1];
+                tabResultat[4] =tab[2][1];
+                tabResultat[5] =tab[3][1];
+            }
+        break;
+        case 1:
+                tabResultat[0]=0;
+                tabResultat[1] =tab[0][1];
+                tabResultat[2] =tab[1][1];
+                tabResultat[3] =tab[2][1];
+                tabResultat[4] =tab[3][1];
+                tabResultat[5] =tab[4][1];
+        break;
+    }
+
+    //codage dans un tableau d'entier (faudrait faire un tableau de quadret parce que
+    //stoquer des nombres qui ne depasse pas 14 sur 32bits c'est gros
+    //le premier indice du tableau correspond au code de la forme
+    //vous l'aurez compris (paire brelan carrÃ©...)
+    //les 4 dernier elements sont le rang des cartes
 }
 
