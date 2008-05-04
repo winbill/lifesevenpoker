20e760f16bb92bb4519b643402409fe83cf168ca
bengrunt <bengrunt@1204c978-5248-0410-81a0-0bcb81ba3628>
2008-05-04 20:47:58 +0000

    en fait nan

---
+++ main.C
@@ -0,0 +1,391 @@
+//Librairies Life'Seven Poker
+//#include "Jeu.h"
+//#include "PileCarte.h"
+//#include "Table.h"
+//#include "Joueur.h"
+//#include "Jeu.h"
+
+//Librairies Standard.
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "AfficheDynamique.h"
+//#include "AfficheTxt.h"
+//Librairies SDL.
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h> //Gestion des images.
+#include <SDL/SDL_ttf.h> //Gestion des polices True Type Fonts.
+//#include <SDL/SDL_mixer.h> //Gestion du multi channeling audio.
+
+#ifdef __cplusplus
+    #include <cstdlib>
+#else
+    #include <stdlib.h>
+#endif
+#include <SDL/SDL.h>
+
+int main ( /*int argc, char** argv */ )
+{
+    /*printf("ETAPE 0 \n");
+    PileCarte p;Table t;
+    initTable(t);
+    initPileCarte(p);
+    t.pileCarte = &p;
+    Joueur* j1;
+    Joueur* j2;
+    Joueur* j3;
+    Joueur* j4;
+
+    printf("ETAPE 1 \n");
+
+    setMaxJoueurTable(t,6);
+    j1 = creeJoueur();initJoueur(*j1,"j1");
+    afficheInfoJoueur(*j1);
+    j2 = creeJoueur();initJoueur(*j2,"j2");
+    afficheInfoJoueur(*j2);
+    j3 = creeJoueur();initJoueur(*j3,"j3");
+    afficheInfoJoueur(*j3);
+    j4 = creeJoueur();initJoueur(*j4,"j4");
+    afficheInfoJoueur(*j4);
+
+    printf("ETAPE 2 \n");
+
+    ajoutJoueurTable(t,j1);
+    afficheInfoJoueur(*j1);
+    ajoutJoueurTable(t,j2);
+    afficheInfoJoueur(*j2);
+    ajoutJoueurTable(t,j3);
+    afficheInfoJoueur(*j3);
+    ajoutJoueurTable(t,j4);
+    afficheInfoJoueur(*j4);
+
+    int i=0;
+    while(i<10)
+    {
+        printf("%d \t",t.joueur[i]);
+        i++;
+    }
+
+    printf("ETAPE 3 \n");
+
+    afficheInfoTable(t);
+    afficheInfoJoueur(*j1);
+    afficheInfoJoueur(*j2);
+    afficheInfoJoueur(*j3);
+    afficheInfoJoueur(*j4);
+
+
+    supprimeJoueurTable(t,j1);
+
+    //ajoutJoueurTable(t,&j4);
+
+    printf("ETAPE 4 \n");
+
+    afficheInfoJoueur(*j1);
+    afficheInfoJoueur(*j2);
+    afficheInfoJoueur(*j3);
+    afficheInfoJoueur(*j4);
+
+    afficheInfoTable(t);
+
+    printf("ETAPE 5a \n");
+    distribuer2CartesJoueursJeu(t);
+
+    printf("ETAPE 5b \n");
+
+    afficheMainCarte(*getMainJoueur(*j1),"main de j1");
+    afficheMainCarte(*getMainJoueur(*j2),"main de j2");
+    afficheMainCarte(*getMainJoueur(*j3),"main de j3");
+    afficheMainCarte(*getMainJoueur(*j4),"main de j4");
+
+    printf("ETAPE 6 \n");
+
+    joueurDetruit(j1);
+    supprimeJoueurTable(t,j2);
+    joueurDetruit(j2);
+    supprimeJoueurTable(t,j3);
+    joueurDetruit(j3);
+    supprimeJoueurTable(t,j4);
+    joueurDetruit(j4);
+
+    afficheInfoTable(t);
+
+    printf("ETAPE 7 \n");
+
+
+
+    pileCarteLibere(p);
+    tableLibere(t);
+
+
+
+
+    printf("ETAPE 8 \n");
+
+
+
+*/
+
+
+
+
+
+
+
+    // screen attributes
+    const int SCREEN_WIDTH = 1024;
+    const int SCREEN_HEIGHT = 768;
+    const int SCREEN_BPP = 32;
+
+    // initialize SDL video
+    if ( SDL_Init( SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER ) < 0 )
+    {
+        printf( "Unable to init SDL: %s\n", SDL_GetError() );
+        return 1;
+    }
+
+    // make sure SDL cleans up before exit
+    atexit(SDL_Quit);
+
+    // create a new window
+    SDL_Surface* screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_HWSURFACE|SDL_DOUBLEBUF);
+    if ( !screen )
+    {
+        printf("Unable to set %d x %d video: %s\n", SCREEN_WIDTH , SCREEN_HEIGHT , SDL_GetError());
+        return 1;
+    }
+
+    // load an image
+    SDL_Surface* background = SDL_LoadBMP("img/bkgrd.bmp");
+    if (!background)
+    {
+        printf("Unable to load bitmap: %s\n", SDL_GetError());
+        return 1;
+    }
+    SDL_Surface* logo = SDL_LoadBMP("img/logo.bmp");
+    if(!logo)
+    {
+        printf("Unable to load bitmap: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // centre the bitmap on screen
+    SDL_Rect dstrect;
+    dstrect.x = (screen->w - background->w) / 2;
+    dstrect.y = (screen->h - background->h) / 2;
+
+    SDL_Rect dstlogo;
+    dstlogo.x = (screen->w - logo->w ) /2;
+    dstlogo.y = (screen->w - logo->h ) /2;
+
+    //Initialisation de SDL_TTF
+	if( TTF_Init() == -1 )
+	{
+		return false;
+	}
+
+    // program main loop
+    bool done = false;
+    while (!done)
+    {
+        // message processing loop
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            // check for messages
+            switch (event.type)
+            {
+                // exit if the window is closed
+            case SDL_QUIT:
+                done = true;
+                break;
+
+                // check for keypresses
+            case SDL_KEYDOWN:
+                {
+                    // exit if ESCAPE is pressed
+                    if (event.key.keysym.sym == SDLK_ESCAPE)
+                        done = true;
+                    break;
+
+                    if (event.key.keysym.sym == SDLK_RETURN)
+                    {
+                        //La surface du message
+                        SDL_Surface *message = NULL;
+
+                        //Le Font qu'on va utiliser
+                        TTF_Font *font;
+
+                        //La couleur du Font
+                        SDL_Color textColor = { 255, 255, 255 };
+
+                        //Ouverture du Font
+                        font = TTF_OpenFont( "TlwgTypeWriter.ttf", 28 );
+                        //S'il y a une erreur dans le chargement du Font
+                        if( font == NULL )
+                        {
+                            return false;
+                        }
+
+                        //Mise en place du texte sur la surface message
+                        message = TTF_RenderText_Solid( font, "Test pour sdl_ttf", textColor );
+
+                        //S'il y a une erreur dans la mise en place du texte
+                        if( message == NULL )
+                        {
+                            return 1;
+                        }
+
+                        //Application de la surface du message
+                        apply_surface( 0, 200, message, screen );
+
+                        SDL_FreeSurface( message );
+
+                        //Fermeture des Fonts qu'on a utilisé
+                        TTF_CloseFont( font );
+
+                    } //end ENTER
+                } //end SDL_KEYDOWN
+            } // end switch
+        } // end of message processing
+
+        // DRAWING STARTS HERE
+
+        // clear screen
+        SDL_FillRect(screen, 0, SDL_MapRGB(screen->format, 0, 0, 0));
+
+        // draw bitmap
+        SDL_BlitSurface(background, 0, screen, &dstrect);
+        SDL_BlitSurface(logo, 0, screen, &dstlogo);
+
+        // DRAWING ENDS HERE
+
+        // finally, update the screen :)
+        SDL_Flip(screen);
+    } // end main loop
+
+    // free loaded bitmap
+    SDL_FreeSurface(background);
+    SDL_FreeSurface(logo);
+
+
+
+	//On quitte SDL_ttf
+	TTF_Quit();
+
+    // all is well ;)
+    printf("Exited cleanly\n");
+
+    return 0;
+}
+
+    // INITIALISATION DE L'AFFICHAGE, DU SON & DES PERIPHERIQUES DE CONTROLE
+
+    //INTRO :
+        // Logo lifesevenpoker , petit jingle ...
+
+    //AFFICHAGE DU MENU PRINCIPAL:
+        //UN JOUEUR
+        //MULTIJOUEURS
+        //OPTIONS
+        //CREDITS
+        //QUITTER
+
+    // + musique ambiance ? sons particuliers sur clicks ?
+
+
+    //=> UN JOUEUR
+        // Fait démarrer une nouvelle partie contre IA.
+        // REGLAGES PREALABLES : Nombre IA, difficulté IA. Argent de départ. Peut etre d'autres options liées aux regles ??
+
+    //=> MULTIJOUEUR
+        // Formulaire pour Creer/rejoindre une partie. Par IP a priori ... détails multijoueurs à voir plus tard ... optionnel.
+
+    //=> OPTIONS
+        //Pouvoir modifier la résolution d'affichage, les raccourcis clavier, niveau sonore musique/effets
+        //  /!\ Utilisation d'un fichier de configuration externe ??? A voir.
+    //=> CREDITS
+        //Affichage des credits mode déroulement ou autre. A priori assez simple.
+
+    //=> QUITTER
+        //Destruction de tout ce qui est chargé en mémoire.
+        //Fermeture de tous les systemes propre.
+        //Fermeture Fenetre.
+
+
+
+
+    /* MODE UN JOUEUR:
+
+
+        BOUCLE DE BASE :
+
+            INITIALISATION DE TOUTES LES DONNEES NECESSAIRE A L'AFFICHAGE
+            AFFICHAGE MESSAGE BIENVENUE / DEBUT DE PARTIE
+
+            AFFICHAGE TABLE DE JEU
+                basique => tapis vert type solitaire windows
+                    noms et argent des joueurs
+
+            GENERATION DE LA PILE DE CARTES
+
+            ATTRIBUTION DES ROLES SELON PLACES :
+                        - DEALER
+                        - PETITE BLIND
+                        - GROSSE BLIND
+                        - JOUEUR
+
+            DISTRIBUTION DES CARTES AUX JOUEURS
+                deux cartes à chaque joueur
+                    animation ? pourquoi pas
+                pas de carte au milieu
+
+            PREMIERE PHASE DE MISES:
+                Cycle : pour chaque joueur demande de mise
+                            prise en compte des blinds
+                            possibilité de se coucher
+                        continue tant que les mises sont pas finies, plusieurs tours de table possibles
+
+            FLOP:
+                3 cartes découvertes au milieu
+                possibilité de se coucher
+
+            DEUXIEME PHASE DE MISES:
+                Cycle : idem
+
+            TURN:
+                4e carte découverte au milieu
+
+            TROISIEME PHASE DE MISES:
+                Cycle ...
+
+            RIVER
+                5e carte découverte au milieu
+
+            DERNIERE PHASE DE MISES:
+                Cycle ...
+
+
+
+                Résolution des mains
+                Determiner gagnant(s)
+                Attribuer gains
+                Sit out si plus d'argent
+
+            REINITIALISATION PILE/MAINS
+
+            RETOUR DEBUT DU CYCLE
+
+
+            //!\\ A TOUT MOMENT : POSSIBILITE DE : SIT OUT, FOLD, QUIT, (ACCES OPTIONS).
+
+
+        */
+
+        //MODE MULTIJOUEUR ASSEZ SIMILAIRE
+
+
+        //GESTION DE L'IA ET DES AUTRES JOUEURS ENCORE A DETERMINER
+
+
+
