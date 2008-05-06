dbb1f9a6d8c2949c2ebe10f6a94ab2a0d2f3c710
jamesdgb <jamesdgb@1204c978-5248-0410-81a0-0bcb81ba3628>
2008-05-06 20:08:27 +0000

    fonction de comparaison des main a tester (hors suite)

--- mainJames.C
+++ mainJames.C
@@ -17,15 +17,78 @@
 #include <SDL/SDL_mixer.h> //Gestion du multi channeling audio.
 
 #ifdef __cplusplus
-    #include <cstdlib>
+#include <cstdlib>
 #else
-    #include <stdlib.h>
+#include <stdlib.h>
 #endif
 #include <SDL/SDL.h>
 
+void afficheTab2(int tab[][2],int x)
+{
+    for (int i=0;i<x;i++)
+    {
+        printf("i:%d    :%d      :%d\n",i,tab[i][0],tab[i][1]);
+    }
+}
+
+void afficheTab(int tab[],int x)
+{
+    for (int i=0;i<x;i++)
+    {
+        printf("i:%d    :%d\n",i,tab[i]);
+    }
+}
+
+
+
 int main ( /*int argc, char** argv */ )
 {
-    PileCarte p;Table t;
+
+    int tabMainTotale[7][2];
+    tabMainTotale[0][0] = 3;
+    tabMainTotale[0][1] = 1;
+
+    tabMainTotale[1][0] = 4;
+    tabMainTotale[1][1] = 2;
+
+    tabMainTotale[2][0] = 4;
+    tabMainTotale[2][1] = 3;
+
+    tabMainTotale[3][0] = 4;
+    tabMainTotale[3][1] = 4;
+
+    tabMainTotale[4][0] = 5;
+    tabMainTotale[4][1] = 1;
+
+    tabMainTotale[5][0] = 5;
+    tabMainTotale[5][1] = 2;
+
+    tabMainTotale[6][0] = 13;
+    tabMainTotale[6][1] = 1;
+
+
+
+    int tabO[15];
+    int tabResultat[10][2];
+
+    nombreOcurenceCarte(tabMainTotale,tabO);
+
+    afficheTab(tabO,7);
+
+
+    choixCarteMultiple(tabMainTotale,tabO,tabResultat);
+
+
+
+    afficheTab2(tabResultat,6);
+
+
+
+
+
+    return 0;
+}
+/*  PileCarte p;Table t;
     initTable(t);
     initPileCarte(p);
     t.pileCarte = &p;
@@ -87,7 +150,4 @@ int main ( /*int argc, char** argv */ )
 
 
     pileCarteLibere(p);
-    tableLibere(t);
-
-    return 0;
-}
+    tableLibere(t);*/
