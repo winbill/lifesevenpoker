7b7ff49356e13d9e796b6f136d1a7f444132a5a4
jamesdgb <jamesdgb@1204c978-5248-0410-81a0-0bcb81ba3628>
2008-05-12 19:39:22 +0000

    l'ia.c que javais oublie de d'adder

---
+++ IArtificielle.C
@@ -0,0 +1,21 @@
+
+#include "IArtificielle.h"
+
+Statut calculIA(const Table & t,const Joueur & j,int &relance,int & montant)
+{
+
+    relance=0;
+    //printf("mise:%d   montant:%d\n",j.mise,montant);
+    if(j.mise==montant)
+        return CHECK;
+    int a=montant-getMiseJoueur(j);
+    if(a>=getArgentJoueur(j))
+    {
+        return ALL_IN;
+    }
+
+
+
+
+    return CALL;
+}
