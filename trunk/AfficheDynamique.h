a2c1c596be9e50d335da14cc18bcd50365645377
jamesdgb <jamesdgb@1204c978-5248-0410-81a0-0bcb81ba3628>
2008-05-26 16:08:55 +0000

    les commentaires doxygen m'oxygène

--- trunk/AfficheDynamique.h
+++ trunk/AfficheDynamique.h
@@ -37,72 +37,74 @@
 
 /**	@brief détermine le ou les vainqueurs et fait l'affichage en fonction
 	@param [in,out] affichage
+	@param [in,out] langue fichier de langue
 	@param [in] t
+	@param [in] afficheCarteGagnant à true si on doit afficher les cartes du gagnant
 */
 void affAffichageVainqueur(SDL_Surface* affichage,Table & t,const char langue[][50],bool afficheCarteGagnant);
 
 
-/**	@brief
-	@param t
-	@param tabResultat
-	@note
+/**	@brief reparti l'argent entre les joueurs, est appelée si le meilleur joueur a fait un tapis
+	@param t table
+	@param tabResultat tableau a 3dimension contenant les vainqueurs
+	@note aucune
 */
 void calulVainqueurTapis(Table & t,int tabResultat[][6][2]);
 
 
-/**	@brief
-	@param source
-	@param destination
-	@param option
-	@return SDL_Rect
+/**	@brief fonction de centrage
+	@param source sur quoi on doit centrer
+	@param destinationce qui doit etre centré
+	@param option de centrage 0 : centrage h et v  1 : centrage h,2 : centrage v
+	@return SDL_Rect contient les coordonnées de centrage
 	@note
 */
 SDL_Rect AffCentrer(SDL_Surface* source, SDL_Surface* destination, int option);
 
 
-/**	@brief
+/**	@brief affiche le logo du debut
 	@param affichage
-	@param tapis
-	@note
+	@param tapis le fond
+	@note 
 */
 void AffStartUp(SDL_Surface* affichage,SDL_Surface* tapis);
 
 
-/**	@brief
+/**	@brief affiche le pot
 	@param affichage
-	@param t
-	@param langue
+	@param t table
+	@param langue fichier de langue
 	@note
 */
 void AffAffichePot(SDL_Surface* affichage,const Table & t,const char langue[][50]);
 
 
-/**	@brief
+/**	@brief affiche le menu
 	@param affichage
-	@param [in] langue
-	@return int
+	@param [in] langue fçichier de langue
+	@return int renvoit un code qui correspond à ceux sur quoi l'on a clique
 	@note
 */
 int AffMenu(SDL_Surface* affichage,const char langue[][50]);
 
 
-/**	@brief
+/**	@brief affiche les infos des joueurs (non locaux)
 	@param affichage
-	@param j
+	@param j le joueurZ
 	@param table
-	@param joueurJouant
-	@param langue
+	@param joueurJouant le joueur a qui c'est au tour de jouer
+	@param langue fichier de langue
 	@note
 */
 void AffAfficheJoueur(SDL_Surface* affichage,const Joueur & j,const Table & table,int joueurJouant,const char langue[][50]);
 
 
-/**	@brief
+/**	@brief affiche des infos sur tous les joueurs et les dispose suivant leurs nombre sur la table
 	@param affichage
-	@param t
-	@param joueurJouant
-	@param langue
-	@note
+	@param t table
+	@param joueurJouant le joueur a qui c'est au tour de jouer
+	@param langue fichier de langue
+	@note appel AffAfficheJoueur
 */
 void AffAffichageInfosJoueurs(SDL_Surface* affichage,const Table & t,int joueurJouant,const char langue[][50]);
 
@@ -124,78 +126,77 @@ void AffCartesJoueursJeu(SDL_Surface* affichage,const Table & t);
 void AffCartesJoueursJeu(SDL_Surface* affichage,const Table & t,bool cache);
 
 
-/**	@brief
-	@param
-	@param
-	@param
-	@return
-	@note
+/**	@brief affiche les cartes qui sont disposées sur le tapis
+	@param t table
+	@param affichage 
+	@param evidence
+	@note appel la fonction AffCarteDecouvertes(const Table & t,SDL_Surface* affichage,bool evidence,int tabResultat[6][2]);
 */
 void AffCarteDecouvertes(const Table & t,SDL_Surface* affichage);
 
 
-/**	@brief
+/**	@brief affiche les cartes qui sont disposées sur le tapis, si evidence est à true affiche en évidence celle présente dans le tableau
 	@param affichage
-	@param evidence
-	@param tabResultat
+	@param evidence à true pour mettre les cartes en évidence
+	@param tabResultat contient les cartes à mettre en évidence
 	@note
 */
 void AffCarteDecouvertes(const Table & t,SDL_Surface* affichage,bool evidence,int tabResultat[6][2]);
 
 
-/**	@brief affiche les cartes des joueurs face cachée (sauf les siennes)
+/**	@brief affiche les cartes du gagnant en surbrillance
 	@param [in,out] affichage
 	@param [in] t
 	@param tabResultat
-	@param i
+	@param i id du gagnant
 	@note
 */
 void AffCartesJoueursJeuFinale(SDL_Surface* affichage,const Table & t,int tabResultat[6][2],int i);
 
 
-/**	@brief
+/**	@brief affichage des infos perso du joueur local
 	@param affichage
 	@param j
 	@param table
-	@param langue
+	@param langue fichier de langue
 	@note
 */
 void AffInfosJoueur(SDL_Surface* affichage,const Joueur &j,const Table & table,const char langue[][50]);
 
 
-/**	@brief
+/**	@brief gestion de l'affichage du bouton relance
 	@param affichage
-	@param relance
-	@param dessus
+	@param relance valuer de la relance
+	@param dessus est ce que la souris est dessus ?
 	@param langue
 	@note
 */
 void AffAfficheBoutonRelance(SDL_Surface* affichage,int relance,bool dessus,const char langue[][50]);
 
 
-/**	@brief
-	@param affichage
-	@param relance
-	@param s
-    @param montant
-	@param j
-	@param t
+/**	@brief scan les actions du joueur, affiche des boutons pour récupérer ces actions
+	@param affichage 
+	@param relance 
+	@param s statut
+	@param montant mise maximale
+	@param j joueur
+	@param t table
 	@param langue
-	@return int
+	@return renvoit un entier au cas ou il clique sur le menu
 	@note
 */
 int scanActionJoueur(SDL_Surface* affichage,int & relance,Statut & s,int & montant,const Joueur & j,const Table & t,const char langue[][50]);
 
 
-/**	@brief
-	@param t
+/**	@brief met l'ensemble des mises dans le pot
+	@param t table
 	@note
 */
 void miseDansPot(Table & t);
 
 
-/**	@brief
-	@param t
+/**	@brief calcul combien gagnge au maximum un joueur lorsquil fait un tapis
+	@param t table
 	@note
 */
 void calculGainTapisJoueur(Table & t);
